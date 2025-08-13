5d729eb0fa0d501d591f31f2640227131c95a004
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2025-08-13 12:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (65.26%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public void flatten(TreeNode root) {
+        makeLL(root);
+    }
+
+    public TreeNode makeLL(TreeNode root) {
+        if(root == null) return null;
+        if(root.left == null && root.right == null) return root;
+        TreeNode left_tail = makeLL(root.left);
+        TreeNode right_tail = makeLL(root.right);
+        if (root.left != null) {
+            left_tail.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        return right_tail != null ? right_tail : left_tail;
+    }
+}
