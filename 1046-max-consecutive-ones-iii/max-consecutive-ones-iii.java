c329bce7a1905657e9cc4f7fd33824d0d9af06cc
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2025-08-28 19:19:19 +0530

    Time: 3 ms (95.00%) | Memory: 52.8 MB (39.04%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -4,9 +4,13 @@ class Solution {
         while (r < nums.length) {
             if (nums[r] == 0)
                 zeros++;
-            while (zeros > k) {
-                if (nums[l] == 0)
-                    zeros--;
+            // while (zeros > k) {
+            //     if (nums[l] == 0)
+            //         zeros--;
+            //     l++;
+            // }
+            if(zeros > k){
+                if(nums[l] == 0) zeros--;
                 l++;
             }
             if (zeros <= k) {
