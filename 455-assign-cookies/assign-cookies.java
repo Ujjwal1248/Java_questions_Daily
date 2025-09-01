4b8bbe1d413da3bb1cff20a0fd1496b3ce988745
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2025-09-01 17:21:25 +0530

    Time: 9 ms (98.70%) | Memory: 48.6 MB (45.60%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -4,15 +4,12 @@ class Solution {
         Arrays.sort(s);
         int m = g.length;
         int n = s.length;
-        int l = 0, r=0;
-        while(l < m && r < n){
-            if(g[l] <= s[r]){
+        int l = 0, r = 0;
+        while (l < m && r < n) {
+            if (g[l] <= s[r]) {
                 l++;
-                r++;
             }
-            else{
-                r++;
-            }
+            r++;
         }
         return l;
     }
