3835b4b859b0052978b639205aa7376c28667e32
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2025-09-01 21:54:13 +0530

    Time: 3 ms (79.70%) | Memory: 45.6 MB (87.48%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = 1;
+        right[n-1] = 1;
+        for(int i = 1; i < n; i++){
+            if(nums[i] > nums[i-1]){
+                left[i] = left[i-1] + 1;
+            }
+            else left[i] = 1;
+        }
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] > nums[i+1]){
+                right[i] = right[i+1] + 1;
+            }
+            else right[i] = 1;
+        }
+        int sum = 0;
+        for(int i = 0; i <n;i++){
+            sum += Math.max(left[i], right[i]);
+        }
+        return sum;
+    }
+}
