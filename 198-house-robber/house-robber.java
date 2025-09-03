821e3698a68f25053880dd523a99e12089874c08
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2025-09-03 11:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (46.38%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,7 +2,8 @@ class Solution {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        return robber(nums, 0, dp);
+        // return robber(nums, 0, dp);
+        return robber2(nums, nums.length-1, dp);
     }
 
     public int robber(int[] nums, int i, int[] dp) {
@@ -22,8 +23,8 @@ class Solution {
         if (dp[i] != -1) {
             return dp[i];
         }
-        int rob = nums[i] + robber(nums, i - 2, dp);
-        int Dont_rob = robber(nums, i - 1, dp);
+        int rob = nums[i] + robber2(nums, i - 2, dp);
+        int Dont_rob = robber2(nums, i - 1, dp);
         return dp[i] = Math.max(rob, Dont_rob);
     }
 }
