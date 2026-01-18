fee5bad089de9ced25bb3a2a27008b3486a67f67
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-01-18 13:51:17 +0530

    Time: 3 ms (77.23%) | Memory: 46.5 MB (65.79%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() < t.length()) return "";
-
-        int n = s.length();
-        int m = t.length();
-        int[] hash = new int[256];  // Array to store frequency of chars in t
-        for (int i = 0; i < m; i++) {
-            hash[t.charAt(i)]++;
+        int[] freqs = new int[256];
+        int[] freqt = new int[256];
+        for (int i = 0; i < t.length(); i++) {
+            freqt[t.charAt(i)]++;
         }
-
-        int l = 0, r = 0;
-        int count = 0;       // Number of chars matched so far
-        int minLen = Integer.MAX_VALUE;
-        int startIndex = -1;
-
-        while (r < n) {
-            // If current char is needed, decrease its count in hash and increase matched count
-            if (hash[s.charAt(r)] > 0) count++;
-            hash[s.charAt(r)]--;
-            r++;
-
-            // When all chars are matched, try to shrink from left
-            while (count == m) {
-                if (r - l < minLen) {
-                    minLen = r - l;
-                    startIndex = l;
-                }
-                
-                hash[s.charAt(l)]++;
-                if (hash[s.charAt(l)] > 0) count--;
-                l++;
+        int si = 0, ei = 0, count = 0, size = Integer.MAX_VALUE, start_Index = -1;
+        while (ei < s.length()) {
+            //Grow
+            char ch = s.charAt(ei);
+            freqs[ch]++;
+            if (freqs[ch] <= freqt[ch]) {
+                count++;
             }
-        }
 
-        return startIndex == -1 ? "" : s.substring(startIndex, startIndex + minLen);
+            //Shrink
+            while (count == t.length() && freqs[s.charAt(si)] > freqt[s.charAt(si)]) {
+                freqs[s.charAt(si)]--;
+                si++;
+            }
+
+            //Update ans
+            if(count == t.length() && size > ei - si +1){
+                size = ei - si +1;
+                start_Index = si;
+            }
+            ei++;
+        }
+        if(start_Index == -1) return "";
+        return s.substring(start_Index, start_Index + size);
     }
 }
