b4f6ec1d8c30b99be89c5c845c29aa645e486d00
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-01-18 22:08:43 +0530

    Time: 23 ms (19.06%) | Memory: 98 MB (72.72%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -5,15 +5,15 @@ class Solution {
         solve(n, k, 1, temp);
         return ans;
     }
-    public void solve(int n, int k, int idx, List<Integer> temp){
+    public void solve(int n, int k, int start, List<Integer> temp){
         if(k==0){
             ans.add(new ArrayList<>(temp));
             return;
         }
-        if(idx > n) return;
-        temp.add(idx);
-        solve(n, k-1, idx + 1, temp);
+        if(start > n) return;
+        temp.add(start);
+        solve(n, k-1, start + 1, temp);
         temp.remove(temp.size() - 1);
-        solve(n, k, idx + 1, temp);
+        solve(n, k, start + 1, temp);
     }
 }
