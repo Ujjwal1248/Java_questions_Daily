d57627508d3bce43de38e2deda918ec1eb7e869c
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-01-19 10:40:39 +0530

    Time: 3 ms (63.28%) | Memory: 43.7 MB (54.22%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,7 +1,6 @@
 class Solution {
     public String makeGood(String s) {
         Stack<Character> st = new Stack<>();
-        int i  = 0;
         for (char c : s.toCharArray()) {
             if (!st.isEmpty() && Math.abs(c - st.peek()) == 32) {
                 st.pop();
