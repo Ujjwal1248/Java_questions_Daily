1c5e7649b124bc044560f1d98d0972cdbe15a68a
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-01-20 00:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (15.31%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,30 +1,34 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-        boolean[] used = new boolean[nums.length];
-        List<Integer> temp = new ArrayList<>();
-        permutation(nums, temp, used);
+        // boolean[] used = new boolean[nums.length];
+        permutation(nums, 0);
         return ans;
     }
 
-    public void permutation(int[] nums, List<Integer> temp, boolean[] used){
-        if(temp.size() == nums.length){
-            ans.add(new ArrayList<>(temp));
+    public void permutation(int[] nums, int idx) {
+        List<Integer> temp = new ArrayList<>();
+        if (idx == nums.length) {
+            for(int num : nums){
+                temp.add(num);
+            }
+            ans.add(temp);
             return;
         }
-        for(int i = 0; i < nums.length; i++){
-            if(used[i]) continue;
-            used[i] = true;
-            // swap(nums, i, idx);
-            temp.add(nums[i]);
-            permutation(nums, temp, used);
-            temp.remove(temp.size() - 1);
-            // swap(nums, i, idx);
-            used[i] = false;
+        for (int i = idx; i < nums.length; i++) {
+            // if(used[i]) continue;
+            // used[i] = true;
+            // temp.add(nums[i]);
+            swap(nums, i, idx);
+            permutation(nums, idx + 1);
+            swap(nums, i, idx);
+            // temp.remove(temp.size() - 1);
+            // used[i] = false;
         }
     }
 
-    public void swap(int[] nums, int i, int j){
+    public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
