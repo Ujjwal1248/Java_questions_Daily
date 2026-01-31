1529cbcfa073e0664c49cc6dc0a4e7fa158dd27d
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-01-31 23:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (61.51%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans = 1.0;
-        long N = n;
-        if (n < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        while (N > 0) {
-            if (N % 2 != 0) {
-                ans *= x;
-                N--;
-            } else {
-                N /= 2;
-                x *= x;
-            }
-        }
-        return ans;
+        return Math.pow(x, n);
     }
 }
