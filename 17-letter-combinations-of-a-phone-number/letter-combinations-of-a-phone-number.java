899e2cf48c1802fa555151ac8d03649870950df9
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-02-02 00:10:19 +0530

    Time: 3 ms (33.09%) | Memory: 49.3 MB (9.57%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         String[] mapping = {
-            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+                "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
         };
         List<String> result = new ArrayList<>();
-        solve(digits, "", mapping, 0,result);
+        solve(digits, "", mapping, 0, result);
         return result;
     }
-    public void solve(String digits, String curr, String[] mapping, int idx, List<String> result){
-        if(idx == digits.length()){
+
+    public void solve(String digits, String curr, String[] mapping, int idx, List<String> result) {
+        if (idx == digits.length()) {
             result.add(curr);
             return;
         }
         int number = digits.charAt(idx) - '0';
         String str = mapping[number];
-        for(int i = 0; i<str.length(); i++){
-
-            solve(digits, curr + str.charAt(i), mapping, idx+1, result);
+        for (int i = 0; i < str.length(); i++) {
+            solve(digits, curr + str.charAt(i), mapping, idx + 1, result);
         }
     }
 }
