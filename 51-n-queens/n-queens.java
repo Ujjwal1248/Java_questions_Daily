3c7eb684b3416953bef3b0a5f8a4d804ef343151
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-02-03 00:28:55 +0530

    Time: 2 ms (79.96%) | Memory: 46.9 MB (12.26%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,43 +1,46 @@
 class Solution {
+    List<List<String>> ans = new ArrayList<>();
+
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
         for (int i = 0; i < n; i++) {
             Arrays.fill(board[i], '.');
         }
-        List<List<String>> ans = new ArrayList<>();
-        nQueens(board, 0, n, ans);
+        helper(board, 0, n);
         return ans;
     }
 
-    public void nQueens(char[][] board, int row, int n, List<List<String>> ans) {
+    public void helper(char[][] board, int row, int n) {
         if (row == n) {
             List<String> temp = new ArrayList<>();
-            for (char[] r : board) {
-                temp.add(new String(r));
+            for (char[] a : board) {
+                temp.add(new String(a));
             }
             ans.add(temp);
             return;
         }
+
         for (int i = 0; i < n; i++) {
             if (isSafe(board, row, i, n)) {
                 board[row][i] = 'Q';
-                nQueens(board, row + 1, n, ans);
+                helper(board, row + 1, n);
                 board[row][i] = '.';
             }
         }
     }
 
-    public boolean isSafe(char[][] board, int row, int col, int n){
-        for(int i = 0; i < row; i++){
-            if(board[row][i] == 'Q' || board[i][col] == 'Q'){
+    public boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
                 return false;
-            }
         }
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+            if (board[i][j] == 'Q')
+                return false;
         }
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q') return false;
+            if (board[i][j] == 'Q')
+                return false;
         }
         return true;
     }
