1633bd5e163469befeb4bb9995fbf71f073b344c
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-02-02 23:42:31 +0530

    Time: 128 ms (87.51%) | Memory: 43.2 MB (27.96%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,29 +1,26 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i =0; i< board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0)){
-                    if(dfs(board, word, i, j, 0)){
-                        return true;
-                    }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (word.charAt(0) == board[i][j]) {
+                    if(helper(board, word, i, j, 0)) return true;
                 }
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board, String word, int r, int c, int idx){
-        if(idx == word.length()){
+
+    public boolean helper(char[][] board, String word, int i, int j, int idx) {
+        if (idx == word.length())
             return true;
-        }
-
-        if(r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != word.charAt(idx)){
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx))
             return false;
-        }
 
-        char temp = board[r][c];
-        board[r][c] = '*';
-        boolean found = dfs(board, word, r+1, c, idx+1) || dfs(board, word, r-1, c, idx+1) || dfs(board, word, r, c+1, idx+1) || dfs(board, word, r, c-1, idx+1);
-        board[r][c] = temp;
-        return found;
+        char temp = board[i][j];
+        board[i][j] = '*';
+        boolean result = helper(board, word, i + 1, j, idx + 1) || helper(board, word, i, j + 1, idx + 1)
+                || helper(board, word, i - 1, j, idx + 1) || helper(board, word, i, j - 1, idx + 1);
+        board[i][j] = temp;
+        return result;
     }
 }
