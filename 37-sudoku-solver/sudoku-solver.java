d4b1179bb99e35bf84608c2711277c15db02d5ac
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-02-04 00:01:07 +0530

    Time: 168 ms (78.37%) | Memory: 43.1 MB (26.88%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -5,8 +5,8 @@ class Solution {
 
     public boolean Sudoku(char[][] board, int row, int col) {
         if (col == 9) {
-            row++;
             col = 0;
+            row++;
         }
         if (row == 9) {
             return true;
@@ -14,33 +14,35 @@ class Solution {
         if (board[row][col] != '.') {
             return Sudoku(board, row, col + 1);
         } else {
-            for (int val = 1; val <= 9; val++) {
-                if (isSafe(board, row, col, val)) {
-                    board[row][col] = (char)(val + '0');
-                    if(Sudoku(board, row, col + 1)) return true;
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
+                    if (Sudoku(board, row, col + 1))
+                        return true;
                     board[row][col] = '.';
                 }
             }
-        return false;
+            return false;
         }
     }
 
     public boolean isSafe(char[][] board, int row, int col, int val) {
-        char ch = (char)(val + '0');
+        char ch = (char) (val + '0');
         for (int i = 0; i < 9; i++) {
-            if (board[i][col] == ch || board[row][i] == ch) {
+            if (board[row][i] == ch || board[i][col] == ch)
                 return false;
-            }
         }
         int r = row - row % 3;
         int c = col - col % 3;
+
         for (int i = r; i < r + 3; i++) {
             for (int j = c; j < c + 3; j++) {
-                if (board[i][j] == ch) {
+                if (board[i][j] == ch)
                     return false;
-                }
             }
         }
+
         return true;
     }
+
 }
