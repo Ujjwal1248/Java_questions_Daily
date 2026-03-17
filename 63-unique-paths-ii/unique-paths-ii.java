8ff34cf720f43e8f895c9742f1477f98ba81f0a1
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-03-17 23:08:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (60.26%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        if(matrix[m-1][n-1] == 1) return 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return helper(m, n, 0, 0, dp, matrix);
+
+    }
+
+    public static int helper(int m, int n, int i, int j, int[][] dp, int[][] matrix) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i >= m || j >= n || matrix[i][j] == 1)
+            return 0;
+        return dp[i][j] = helper(m, n, i + 1, j, dp, matrix) + helper(m, n, i, j + 1, dp, matrix);
+    }
+}
