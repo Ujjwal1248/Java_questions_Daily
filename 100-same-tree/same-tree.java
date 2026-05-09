d0d44d351ba136a0b029cade1be78bc6b3fb42ca
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-05-09 22:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (33.34%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,7 +15,14 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null || q == null) return p==q;
-        return p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return helper(p, q);
+    }
+    public boolean helper(TreeNode p, TreeNode q){
+        if(p == null && q == null) return true;
+        if(p == null || q== null) return false;
+        if(p.val != q.val) return false;
+        boolean pL = helper(p.left, q.left);
+        boolean pR = helper(p.right, q.right);
+        return pL && pR;
     }
 }
