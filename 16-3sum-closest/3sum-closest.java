d00cb46a2dff1fcf6afc2054d0863222d4ff8378
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-07-21 22:36:16 +0530

    Time: 18 ms (41.27%) | Memory: 45.4 MB (81.02%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -4,8 +4,8 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            // if (i > 0 && nums[i] == nums[i - 1])
+            //     continue;
             int j = i + 1;
             int k = n - 1;
             while (j < k) {
