645c4d08dcb8d9c44f8b546e3cd12bff4efaf222
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-08-23 21:28:39 +0530

    Time: 110 ms (37.57%) | Memory: 47.4 MB (5.54%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,21 +1,21 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int[][] dp = new int[nums.length + 1][2002];
-        for (int i = 0; i <= nums.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        HashMap<String, Integer> dp = new HashMap<>();
         return helper(nums, target, 0, dp);
     }
-
-    public int helper(int[] nums, int target, int idx, int[][] dp) {
-        if (idx == nums.length) {
-            return target==0 ? 1 : 0;
+    public int helper(int[] nums, int target, int idx, HashMap<String, Integer> dp) {
+        if(idx == nums.length){
+            if(target == 0) return 1;
+            return 0;
         }
-        if (target < -1000 || target > 1000) return 0;
-        if (dp[idx][target + 1000] != -1)
-            return dp[idx][target+1000];
-        int sub = helper(nums, target - nums[idx], idx + 1, dp);
-        int add = helper(nums, target + nums[idx], idx + 1, dp);
-        return dp[idx][target + 1000] = sub + add;
+        String key = idx + "," + target;
+        if(dp.containsKey(key)) return dp.get(key); 
+
+        int plus = helper(nums, target - nums[idx], idx + 1, dp);
+        int minus = helper(nums, target + nums[idx], idx + 1, dp);
+
+        int ans = plus + minus;
+        dp.put(key, ans);
+        return ans;
     }
 }
