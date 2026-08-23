7191a9b06ad01ef2311ceb3e23bce1d82812cb5b
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-08-23 18:13:40 +0530

    Time: 98 ms (18.00%) | Memory: 75.7 MB (18.04%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,32 +1,24 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i = 0; i<nums.length; i++){
-            sum+=nums[i];
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
         }
         if(sum % 2 == 1) return false;
-        sum/=2;
-        int[][] dp = new int[nums.length+1][sum+1];
-        for(int i = 0; i<=nums.length; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return helper(nums, 0, 0, sum, dp);
+        sum /= 2;
+        Boolean[][] dp = new Boolean[nums.length][sum+1];
+        return helper(nums, sum, 0, dp);
     }
-
-    public boolean helper(int[] nums, int sum1, int idx, int sum, int[][] dp) {
-        if (sum == sum1){
-            dp[idx][sum1] = 1;
-            return true;
+    static boolean helper(int arr[], int sum, int idx, Boolean[][] dp) {
+        if(sum == 0) return true;
+        if(idx >= arr.length) return false;
+        if(dp[idx][sum] != null) return dp[idx][sum];
+        
+        boolean notTake = helper(arr, sum, idx + 1, dp);
+        boolean take = false;
+        if(sum >= arr[idx]){
+            take = helper(arr, sum - arr[idx], idx + 1, dp);
         }
-        if (idx >= nums.length || sum1 > sum){
-            return false;
-        }
-        if(dp[idx][sum1] == 1) return true;
-        else if(dp[idx][sum1] == 0) return false;
-        boolean ans1 = helper(nums, sum1 + nums[idx], idx + 1, sum, dp);
-        boolean ans2 = helper(nums, sum1, idx + 1, sum, dp);
-        if(ans1 == true || ans2 == true) dp[idx][sum1] = 1;
-        else dp[idx][sum1] = 0;
-        return ans1 || ans2;
+        return dp[idx][sum] = notTake || take;
     }
 }
