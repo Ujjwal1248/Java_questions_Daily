ed66201f0e90bc9b5abb76e293eba4e03ee099e3
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-09-07 16:46:53 +0530

    Time: 1 ms (81.08%) | Memory: 43.3 MB (33.54%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,18 +1,23 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        Integer[] dp = new Integer[s.length()];
+        return helper(s, 0, dp);
+    }
+    public int helper(String s, int idx, Integer[] dp){
 
-        for (int i = 2; i <= n; i++) {
-            int one = Integer.parseInt(s.substring(i - 1, i));
-            int two = Integer.parseInt(s.substring(i - 2, i));
-            if (one >= 1 && one <= 9) dp[i] += dp[i - 1];
-            if (two >= 10 && two <= 26) dp[i] += dp[i - 2];
+        if(idx == s.length()) return 1;
+        if(s.charAt(idx) == '0') return 0;
+        if(dp[idx] != null) return dp[idx];
+
+        int oneP = helper(s, idx +1, dp);
+        int two = 0;
+        if(idx + 1 < s.length()){
+            int num = Integer.parseInt(s.substring(idx, idx+2));
+            if(num >= 10 && num <= 26){
+                two = helper(s, idx + 2, dp);
+            }
         }
 
-        return dp[n];
+        return dp[idx] = oneP + two;
     }
 }
