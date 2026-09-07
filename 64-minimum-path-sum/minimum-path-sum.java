0075d57685442994eb0c5b86e8c4b0b483194083
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-09-07 19:21:11 +0530

    Time: 3 ms (72.59%) | Memory: 49.5 MB (97.65%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,27 +1,25 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return helper(m, n, 0, 0, dp, grid);
+    public int minPathSum(int[][] nums) {
+        int n = nums.length, m = nums[0].length;
+        Integer[][] dp = new Integer[n + 1][m + 1];
+        return helper(nums, m, n, 0, 0, dp);
     }
 
-    public static int helper(int m, int n, int i, int j, int[][] dp, int[][] grid) {
-        if (i >= m || j >= n)
+    public int helper(int[][] nums, int m, int n, int i, int j, Integer[][] dp) {
+        if (i >= n || j >= m)
             return Integer.MAX_VALUE;
-        if (i == m - 1 && j == n - 1) {
-            return grid[i][j];
+        if (i == n - 1 && j == m - 1) {
+            return nums[i][j];
         }
-        if (dp[i][j] != -1)
+        if (dp[i][j] != null)
             return dp[i][j];
-        int down = helper(m, n, i + 1, j, dp, grid);
-        int right = helper(m, n, i, j + 1, dp, grid);
-        int min = Math.min(down, right);
-
-        if(min == Integer.MAX_VALUE) return dp[i][j] = min;
-        return dp[i][j] = grid[i][j] + min;
+        int right = Integer.MAX_VALUE, down = Integer.MAX_VALUE;
+        if (i+1 < n) {
+            right = nums[i][j] + helper(nums, m, n, i + 1, j, dp);
+        }
+        if (j+1 < m) {
+            down = nums[i][j] + helper(nums, m, n, i, j + 1, dp);
+        }
+        return dp[i][j] = Math.min(right, down);
     }
 }
