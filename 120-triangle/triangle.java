2c33f4ef1e883eaf0fb2a0f07e5c772a4565fe69
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-09-07 19:46:56 +0530

    Time: 1 ms (99.92%) | Memory: 46.6 MB (12.92%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,20 +1,18 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        Integer[][] dp = new Integer[triangle.size()][triangle.size()];
-        // for (int i = 0; i < triangle.size(); i++) {
-        //     Arrays.fill(dp[i], -1);
-        // }
-        return helper(triangle, 0, 0, dp);
+    public int minimumTotal(List<List<Integer>> nums) {
+        int n = nums.size(), m = nums.get(n - 1).size();
+        Integer[][] dp = new Integer[n + 1][m + 1];
+        return helper(nums, m, n, 0, 0, dp);
     }
 
-    public int helper(List<List<Integer>> triangle, int i, int j, Integer[][] dp) {
-        if (i == triangle.size() - 1)
-            return triangle.get(i).get(j);
+    public int helper(List<List<Integer>> nums, int m, int n, int i, int j, Integer[][] dp) {
+        if (i == n)
+            return 0;
         if (dp[i][j] != null)
             return dp[i][j];
-        int cr = triangle.get(i).get(j);
-        int left = cr + helper(triangle, i + 1, j, dp);
-        int right = cr + helper(triangle, i + 1, j + 1, dp);
+
+        int left = nums.get(i).get(j) + helper(nums, m, n, i + 1, j, dp);
+        int right = nums.get(i).get(j) + helper(nums, m, n, i + 1, j + 1, dp);
 
         return dp[i][j] = Math.min(left, right);
     }
