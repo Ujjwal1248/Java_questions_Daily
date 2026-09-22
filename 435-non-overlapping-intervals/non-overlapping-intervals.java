b30ece3d81e6e86f8175505178a17265a140da58
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-09-22 15:56:48 +0530

    Time: 45 ms (85.44%) | Memory: 115.7 MB (75.94%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,33 +1,27 @@
 class Solution {
-    class Pair {
-        int start, end;
-        Pair(int s, int e) {
-            this.start = s;
-            this.end = e;
+    class Pair{
+        int start;
+        int end;
+        Pair(int start, int end){
+            this.start = start;
+            this.end = end;
         }
     }
-
-    public int eraseOverlapIntervals(int[][] intervals) {
-        int n = intervals.length;
-        if (n == 0) return 0;
-
-        Pair[] meeting = new Pair[n];
-        for (int i = 0; i < n; i++) {
-            meeting[i] = new Pair(intervals[i][0], intervals[i][1]);
+    public int eraseOverlapIntervals(int[][] nums) {
+        int n = nums.length;
+        Pair[] pairs = new Pair[n];
+        for(int i = 0; i <n; i++){
+            pairs[i] = new Pair(nums[i][0], nums[i][1]);
         }
-
-        Arrays.sort(meeting, (a, b) -> a.end - b.end);
-
-        int count = 1;
-        int lastEnd = meeting[0].end;
-
-        for (int i = 1; i < n; i++) {
-            if (meeting[i].start >= lastEnd) {
+        Arrays.sort(pairs, (a,b) -> a.end - b.end);
+        int lastEnd = pairs[0].end;
+        int count = 0;
+        for(int i = 1; i < n; i++){
+            if(pairs[i].start < lastEnd){
                 count++;
-                lastEnd = meeting[i].end;
             }
+            else lastEnd = pairs[i].end;
         }
-
-        return n - count;
+        return count;
     }
 }
