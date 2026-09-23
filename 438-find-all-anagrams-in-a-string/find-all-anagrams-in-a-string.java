8725213ec596cda9cb4d1f2a1c425f2eef8335cf
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-09-23 23:14:51 +0530

    Time: 12 ms (52.14%) | Memory: 47.1 MB (36.60%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,18 +1,22 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-        if(p.length() > s.length()) return ans;
-        int[] aS = new int[26];
-        int[] aP = new int[26];
-        for(int i = 0; i < p.length(); i++){
-            aS[s.charAt(i) - 'a']++;
-            aP[p.charAt(i) - 'a']++;
+        if (p.length() > s.length())
+            return ans;
+        int[] freqS = new int[26];
+        int[] freqP = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            freqS[s.charAt(i) - 'a']++;
+            freqP[p.charAt(i) - 'a']++;
         }
-        if(Arrays.equals(aS, aP)) ans.add(0);
-        for(int i = p.length(); i < s.length(); i++){
-            aS[s.charAt(i) - 'a']++;
-            aS[s.charAt(i - p.length()) - 'a']--;
-            if(Arrays.equals(aS, aP)) ans.add(i - p.length() + 1);
+        if (Arrays.equals(freqS, freqP)) {
+            ans.add(0);
+        }
+        for (int i = p.length(); i < s.length(); i++) {
+            freqS[s.charAt(i) - 'a']++;
+            freqS[s.charAt(i - p.length()) - 'a']--;
+            if (Arrays.equals(freqS, freqP))
+                ans.add(i - p.length() + 1);
         }
         return ans;
     }
